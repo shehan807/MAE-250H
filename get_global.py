cf93f9a9250bcf9e5d5215b4a19f627d374c558f
Shehan Parmar <parmar@g.ucla.edu>
2021-05-25 16:40:39 -0700

    made get_global flexible for receiving input files with single nx/ny/dt values or an array of values

--- get_global.py
+++ get_global.py
@@ -3,23 +3,43 @@ Created on May 2 2021
 @author: Shehan M. Parmar
 Global variables for NS solver. 
 """
-    
-with open(filename, 'r') as inp: 
+import numpy as np
+filename = 'inputs.txt'
+filename = 'inputsGradTest.txt'
+inpFilePath = './InputFiles/'
+with open(inpFilePath + filename, 'r') as inp: 
     
     inputs = {}
     
     for line in inp:
         key = line.split('=')[0].strip()
         attr = line.split('=')[1].strip()
-        inputs[key] = attr
+        if ',' in attr: # applies only for nx, ny, or dt
+            attr = attr.split(',')
+            if ("nx" == key) or ("ny" == key):
+                attr = np.array([int(entry) for entry in attr])
+                inputs[key] = attr
+            elif "dt" == key:
+                attr = np.array([float(entry) for entry in attr])
+                inputs[key] = attr
+            continue
+        inputs[key] = float(attr)
+  
+    if isinstance(inputs["nx"], str):
+        nx = int(inputs["nx"])
+        ny = int(inputs["ny"])
+    elif isinstance(inputs["nx"], np.ndarray):
+        nx = inputs["nx"]
+        ny = inputs["ny"]
     
-    nx = int(inputs["nx"])
-    ny = int(inputs["ny"]) 
+    if len(nx) != len(ny):
+        raise("Inputs in nx and ny MUST match.")
+
     Lx = float(inputs["Lx"])
     Ly = float(inputs["Ly"])
     
     dx = Lx/nx
     dy = Ly/ny
-
+    
     q_size = (nx-1)*ny + nx*(ny-1)
     p_size = nx*ny-1 # subtract one only for pinned pressure values
--- main.py
+++ main.py
@@ -7,41 +7,50 @@ global nx, ny, u, v, p, dx, dy, q_size, p_size
 
 from numpy import linalg as LA
 from get_global import *
-from init import *
-get_global('inputs.txt')
-init()
-print(u)
+
+outFile = 'output'+filename.split('inputs')[-1]
+print(outFile)
 #from init import *
-import operators as op
 
-# Occasionally, np.arange will include the "stop" value due to rounding/floating
-# point error, so a small corrector term (relative to grid spacing) ensures 
-# arrays have correct length
-corrX = 1e-6*dx
-corrY = 1e-6*dy
 
-xu = np.arange(dx, Lx-corrX, dx)
-yu = np.arange(0.5*dy, Ly-corrY, dy)
-Xu, Yu = np.meshgrid(xu, yu)
-Zxu = Yu*np.cos(Xu*Yu)
-grad_x_ex = np.reshape(Zxu, (1, ny*(nx-1)))
 
-xv = np.arange(0.5*dx, Lx-corrX, dx)
-yv = np.arange(dy, Ly-corrY, dy)
-Xv, Yv = np.meshgrid(xv, yv)
-Zyv = Xv*np.cos(Xv*Yv)
-grad_y_ex = np.reshape(Zyv, (1, nx*(ny-1)))
 
-xp = np.arange(0.5*dx, Lx-corrX, dx)
-yp = np.arange(0.5*dy, Ly-corrY, dy)
-Xp, Yp = np.meshgrid(xp, yp)
-Zp = np.sin(Xp*Yp)
 
-grad_ex = np.concatenate((grad_x_ex, grad_y_ex), axis=1)
-g_test = np.reshape(Zp, (1,nx*ny))
-q = op.grad(g_test, pinned=False)
+#get_global('inputs.txt')
 
-err = q-grad_ex
-L2 = LA.norm(err) / len(err[0])
-Linf = LA.norm(err, ord=np.inf)
-print(dx, dy, dx*dy, L2, Linf)
+#init()
+#print(u)
+##from init import *
+#import operators as op
+#
+## Occasionally, np.arange will include the "stop" value due to rounding/floating
+## point error, so a small corrector term (relative to grid spacing) ensures 
+## arrays have correct length
+#corrX = 1e-6*dx
+#corrY = 1e-6*dy
+#
+#xu = np.arange(dx, Lx-corrX, dx)
+#yu = np.arange(0.5*dy, Ly-corrY, dy)
+#Xu, Yu = np.meshgrid(xu, yu)
+#Zxu = Yu*np.cos(Xu*Yu)
+#grad_x_ex = np.reshape(Zxu, (1, ny*(nx-1)))
+#
+#xv = np.arange(0.5*dx, Lx-corrX, dx)
+#yv = np.arange(dy, Ly-corrY, dy)
+#Xv, Yv = np.meshgrid(xv, yv)
+#Zyv = Xv*np.cos(Xv*Yv)
+#grad_y_ex = np.reshape(Zyv, (1, nx*(ny-1)))
+#
+#xp = np.arange(0.5*dx, Lx-corrX, dx)
+#yp = np.arange(0.5*dy, Ly-corrY, dy)
+#Xp, Yp = np.meshgrid(xp, yp)
+#Zp = np.sin(Xp*Yp)
+#
+#grad_ex = np.concatenate((grad_x_ex, grad_y_ex), axis=1)
+#g_test = np.reshape(Zp, (1,nx*ny))
+#q = op.grad(g_test, pinned=False)
+#
+#err = q-grad_ex
+#L2 = LA.norm(err) / len(err[0])
+#Linf = LA.norm(err, ord=np.inf)
+#print(dx, dy, dx*dy, L2, Linf)
